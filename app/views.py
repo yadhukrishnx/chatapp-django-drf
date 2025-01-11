4d918f798926fcfb6206639e2d7cdfb268ac9316
yadhukrishnx <yadhukrishnx@gmail.com>
2025-01-11 11:13:30 +0530

    feat(frontend) : Add Routes , templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def base(request):
+    return render(request,'base.html')
+
+def signup(request):
+    return render(request,'auth/signup.html')
+def login(request):
+    return render(request,'auth/login.html')
+
+def chat(request):
+    return render(request,'home/chat.html')
--- chatapp/settings.py
+++ chatapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+    'django_bootstrap_icons',
     'app',
 ]
 
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
